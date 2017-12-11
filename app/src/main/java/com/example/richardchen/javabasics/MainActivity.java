5eaa23b4c16a8d0f6adbc7a728d85032659a2299
RichardChen <richardchen@via.com.tw>
2017-12-11 14:55:33 +0800

    Add some code for testing

--- app/src/main/java/com/example/richardchen/javabasics/MainActivity.java
+++ app/src/main/java/com/example/richardchen/javabasics/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.richardchen.javabasics;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import java.util.Arrays;
 
+
+public class MainActivity extends Activity {
+    String TAG = "javabasics";
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
@@ -19,6 +23,26 @@ public class MainActivity extends Activity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+//String
+        String s = "NOW";
+        String t = s + " is the time.";
+        String t1 = s + " " + 23.4;
+        char c = t.charAt(2);
+        Log.d(TAG,t1);
+        int len = t1.length();
+        Log.d(TAG,String.valueOf(len));
+//Array
+        char[] text = "Now is the time".toCharArray();
+        char[] copy = new char[100];
+        System.arraycopy(text,4,copy,0,10);
+//        Log.d(TAG,String.valueOf(copy));
+
+        System.arraycopy(text,4,copy,0,10);
+//Byte Array
+        byte[] data = new byte[100];
+        Arrays.fill(data, (byte)-1);
+        Arrays.fill(data, 5,10, (byte)-2);
+
     }
 
     /**
